e8e0cb4bfbf8c3d8802b76dac1fad89e516e0a18
lwesterl <lauri.v.westerholm@gmail.com>
2019-07-27 23:17:41 +0300

    sql file to create test entries to the database

---
+++ backend/db/create_test_entries.sql
@@ -0,0 +1,17 @@
+-- ONLY for testing: insert values to the database
+-- Author: Lauri Westerholm
+
+INSERT INTO Users
+VALUES(1, "Test user 1");
+
+INSERT INTO Users
+VALUES(2, "Test user 2");
+
+INSERT INTO Scores
+VALUES(1, 200, "12-04-2019 15:34", 1, "Level 1", 1);
+
+INSERT INTO Scores
+VALUES(2, 100, "20-05-2019 23:44", 1, "Level 1", 1);
+
+INSERT INTO Scores
+VALUES(3, 10, "01-09-2018 10:19", 1, "Level 2", 2);
