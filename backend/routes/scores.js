d6784aef7ec823501f0ca76e8a4dc5373047dbe8
lwesterl <lauri.v.westerholm@gmail.com>
2019-07-29 21:36:16 +0300

    create basic user related api and modify scores to automatically choose an id

--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -16,7 +16,7 @@ const jsStringEscape = require('js-string-escape');
   *   @return scores in json format
   */
 router.get('/', function(req, res, next) {
-  scores_query = 'SELECT * FROM Scores';
+  scores_query = 'SELECT * FROM Scores;';
   db.select(scores_query, (scores) => {
     res.json(scores);
   });
@@ -26,18 +26,27 @@ router.get('/', function(req, res, next) {
   *   Add one new Score instance to the database
   *   @param req body should contain a new Score as json
   *   @return 200 on success, 403 on failure, see console log
-  *   For testing: curl -X POST -H 'Content-Typetion/json' -d '{ "id": 6, "score": 300, "time": "01-01-2019 21:00", "completed": 1, "level": "Level 3", "userID": 1 }' http://localhost:5000/api/scores/add_score
+  *   Note: id which is used as primary key for Score is automatically choosen
+  *   For testing: curl -X POST -H 'Content-Type:application/json' -d '{ "score": 300, "time": "01-01-2019 21:00", "completed": 1, "level": "Level 3", "userID": 1 }' http://localhost:5000/api/scores/add_score
   */
 router.post('/add_score', function(req, res, next) {
   var new_score = req.body;
-  //var new_score = { id: 5, score: 300, time: '01-01-2019 21:00', completed: 1, level: 'Level 3', userID: 1 };
-  if (('id' in new_score) && ('score' in new_score) && ('time' in new_score) &&
+  if (('score' in new_score) && ('time' in new_score) &&
   ('completed' in new_score) && ('level' in new_score) && ('userID' in new_score)) {
-    if ((isNumeric(new_score.score)) && (isInteger(new_score.id)) &&
-    (new_score.completed === 1 || new_score.completed === 0) && (isInteger(new_score.userID))) {
-      const insert = `INSERT INTO Scores VALUES(${new_score.id}, ${new_score.score}, "${jsStringEscape(new_score.time.trim())}", ${new_score.completed}, "${jsStringEscape(new_score.level.trim())}", ${new_score.userID});`;
-      db.add_entries(insert, (result) => {
-        res.sendStatus(result);
+    if ((isNumeric(new_score.score)) && (new_score.completed === 1 || new_score.completed === 0)
+    && (isInteger(new_score.userID))) {
+      const id_query = 'SELECT MAX(id) as id FROM Scores;';
+
+      db.select(id_query, (id) => {
+        if (Object.keys(id).length === 0) {
+          res.sendStatus(304);
+        } else {
+          var new_id = id[0].id + 1; // this should be unique
+          const insert = `INSERT INTO Scores VALUES(${new_id}, ${new_score.score}, "${jsStringEscape(new_score.time.trim())}", ${new_score.completed}, "${jsStringEscape(new_score.level.trim())}", ${new_score.userID});`;
+          db.add_entries(insert, (result) => {
+            res.sendStatus(result);
+          });
+        }
       });
     } else {
       res.sendStatus(403);
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+/**
+  *   Users related backend api
+  *   @author Lauri Westerholm
+  */
 
-/* GET users listing. */
+const express = require('express');
+const db = require('../db/db');
+const router = express.Router();
+const isNumeric = require('isnumeric');
+const jsStringEscape = require('js-string-escape');
+
+
+/**
+  *   Get all users
+  *   @return all users as json
+  */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const users_query = 'SELECT * FROM Users;';
+  db.select(users_query, (users) => {
+    res.json(users);
+  });
+});
+
+/**
+  *   Get specific user with name as search key
+  *   @return user as json or empty object if not found
+  */
+
+router.get('/user/:name', function(req, res, next) {
+  var name = jsStringEscape(req.params.name);
+   const query = `SELECT * from Users WHERE name="${name}";`;
+  db.select(query, (user) => {
+    res.json(user);
+  });
+});
+
+/**
+  *   Add new user to the database
+  *   @param req body should contain a name for the new User as json
+  *   @return 200 on success, otherwise 403
+  *   Note: id which is used as primary key is automatically choosen
+  *   For testing: curl -X POST -H 'Content-Type:application/json' -d '{ "name": "New user" }' http://localhost:5000/api/users/add_user
+  */
+router.post('/add_user', function(req, res, next) {
+  var new_user = req.body;
+  console.log(new_user);
+  if ('name' in new_user) {
+    name = jsStringEscape(new_user.name);
+    const id_query = 'SELECT MAX(id) as id FROM Users;';
+
+    db.select(id_query, (id) => {
+      if (Object.keys(id).length === 0) {
+        res.sendStatus(304);
+      } else {
+        var new_id = id[0].id + 1; // this should be unique to be used as primary key
+        const insert = `INSERT INTO Users VALUES(${new_id}, "${name}");`;
+        db.add_entries(insert, (result) => {
+          res.sendStatus(result);
+          // if 403 is returned here, unique constrant on name has probably failed
+        });
+      }
+    });
+
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 module.exports = router;
