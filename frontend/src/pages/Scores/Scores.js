dec053fb66bdd02c6ac2d4aac5950ab237d9bb10
lwesterl <lauri.v.westerholm@gmail.com>
2019-08-01 22:32:08 +0300

    add initials for score graph

--- frontend/src/pages/Scores/Scores.js
+++ frontend/src/pages/Scores/Scores.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import Graph from '../../components/Graph'
 import './Scores.css';
 
+/**
+  *   @class Scores
+  *   Implements score visualization using Graph
+  */
 class Scores extends Component {
   constructor() {
     super();
@@ -10,24 +15,48 @@ class Scores extends Component {
   }
 
   componentDidMount() {
+    /** TODO: Scores should be returned newer first */
+    this.updateScores();
+    // automatically refresh scores
+    this.updateInterval = setInterval(() => {
+      this.updateScores();
+    }, 2000)
+  }
+
+  updateScores() {
     fetch('/api/scores')
     .then(res => res.json())
     .then(scores => this.setState({scores}, () => console.log('Scores fetched: ',
-          scores)));
+    scores)));
   }
 
   render() {
-    return (
-      <div>
-        <h1>Scores</h1>
-        <ul>
-          {this.state.scores.map(score => (
-          <li key={score.id}><h5>{score.time}, {score.level}, {score.score}</h5></li>
-        ))}
-        </ul>
-      </div>
-    );
+    var plottable_scores = [];
+    const MaxPlotLen = 100;
+    const MinScoresPlotted = 5;
+    for (var i = 0; i < this.state.scores.length; i++) {
+      if (i < MaxPlotLen) {
+          plottable_scores.push({'x' : i + 1, 'y' : this.state.scores[i].score});
+      }
 
+    }
+    //console.log('plottable_scores: ', plottable_scores);
+    if (plottable_scores.length > MinScoresPlotted) {
+      return (
+        <div>
+          <h1>Scores</h1>
+          <Graph data={plottable_scores}  domain={{ 'x': [1, plottable_scores.length], 'y' : [0, 300]}} title='History'/>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Scores</h1>
+          <p>Not enough scores to plot history
+          <br/> Play more first!</p>
+        </div>
+      );
+    }
   }
 }
 
