46ddabd2e2960ac302f6ace1231b890938871229
lwesterl <lauri.v.westerholm@gmail.com>
2019-08-03 22:36:31 +0300

    plot only user specific scores and initials for stats

--- frontend/src/pages/Scores/Scores.js
+++ frontend/src/pages/Scores/Scores.js
@@ -14,6 +14,7 @@ class Scores extends Component {
 
   _isMounted = false;
 
+
   constructor() {
     super();
     this.state = {
@@ -21,6 +22,8 @@ class Scores extends Component {
       needsRedirect : false,
       scoresReady : false
     }
+    this.MaxPlotLen = 100;
+    this.MinScoresPlotted = 5;
   }
 
 getUser() {
@@ -51,7 +54,7 @@ getUser() {
   }
 
   updateScores() {
-    fetch('/api/scores')
+    fetch(`/api/scores/specific_scores?name=${this.props.match.params.name}&level=get_all_levels`)
     .then(res => res.json())
     .then(scores => this.setState({scores}, () => {
       if (this._isMounted) {
@@ -61,29 +64,44 @@ getUser() {
     }));
   }
 
+  getPlottableScores() {
+    var plottable_scores = [];
+    for (var i = 0; i < this.state.scores.length; i++) {
+        plottable_scores.push({'x' : i + 1, 'y' : this.state.scores[i].score});
+    }
+    if (plottable_scores.length > this.MaxPlotLen) {
+      plottable_scores = plottable_scores.slice(-this.MaxPlotLen, );
+    }
+    return plottable_scores;
+  }
+
+  getScoreStats() {
+    var fails = 0;
+    var successes = 0;
+    var ratio = 100;
+    for (var i = 0; i < this.state.scores.length; i++) {
+      if (this.state.scores[i].completed) successes++;
+      else fails++;
+    }
+    if (fails + successes !== 0) ratio = 100 * successes / (successes + fails);
+    return { 'fails' : fails, 'completed' : successes, 'ratio' : ratio };
+  }
+
 
   render() {
-    const MaxPlotLen = 100;
-    const MinScoresPlotted = 5;
-
     if (this.state.needsRedirect) {
       return <Redirect to='/scores' />
     } else {
-      var plottable_scores = [];
-
-      for (var i = 0; i < this.state.scores.length; i++) {
-          plottable_scores.push({'x' : i + 1, 'y' : this.state.scores[i].score});
-      }
-      if (plottable_scores.length > MaxPlotLen) {
-        plottable_scores = plottable_scores.slice(-MaxPlotLen, );
-      }
+      var plottable_scores = this.getPlottableScores();
+      var stats = this.getScoreStats();
     }
-    //console.log('plottable_scores: ', plottable_scores);
+    console.log('plottable_scores: ', plottable_scores);
     if (this.state.scoresReady) {
-      if (plottable_scores.length > MinScoresPlotted) {
+      if (plottable_scores.length > this.MinScoresPlotted) {
         return (
           <div>
-            <h1>Scores</h1>
+            <h1>{this.props.match.params.name} scores</h1>
+            <p>Completed: {stats.completed}<br/>Fails: {stats.fails}<br/>Success-%: {stats.ratio}</p>
             <Graph data={plottable_scores}  domain={{ 'x': [1, plottable_scores.length], 'y' : [0, 300]}} title='History'/>
           </div>
         );
