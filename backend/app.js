93f7f38e72d5cc921856ce7a7b58e89bc99d70fb
lwesterl <lauri.v.westerholm@gmail.com>
2019-08-07 23:35:00 +0300

    add levels route to backend

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const scoresRouter = require('./routes/scores');
+const levelsRouter = require('./routes/levels');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/scores', scoresRouter);
+app.use('/api/levels', levelsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ backend/routes/levels.js
@@ -0,0 +1,22 @@
+/**
+  *   Levels related backend api
+  *   @author Lauri Westerholm
+  */
+
+  const express = require('express');
+  const db = require('../db/db');
+  const router = express.Router();
+  
+
+  /**
+    *   Get all levels
+    *   @return levels in json format
+    */
+  router.get('/', function(req, res, next) {
+    level_query = 'SELECT name FROM Levels ORDER BY sortKey ASC;';
+    db.select(level_query, (levels) => {
+      res.json(levels);
+    });
+  });
+
+module.exports = router;
