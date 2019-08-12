ac2657c92da98e58f68c87d0f0285529375f6e03
lwesterl <lauri.v.westerholm@gmail.com>
2019-08-12 22:15:11 +0300

    display top scores in ScoresHome (an initial version)

---
+++ frontend/src/components/ScoreTable.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+
+/**
+  *   @class ScoreTable
+  *   Displays scores (top scores)
+  *   props.data: scores to be displayed
+  *   Todo: make much nicer
+  */
+class ScoreTable extends Component {
+
+  /**
+    *   Render ScoreTable
+    *   If no data provided, renders nothing
+    */
+  render() {
+    if (this.props.data !== undefined) {
+      var rows = [];
+      this.props.data.forEach(score => {
+        rows.push(<tr key={score.id}><td>{score.score}</td><td>{score.level}</td></tr>);
+      });
+      return (
+        <div>
+          <h2>All time top scores</h2>
+          <table>
+            <tbody>
+              {rows}
+            </tbody>
+          </table>
+        </div>
+      );
+
+    } else {
+      return (
+        null
+      );
+    }
+  }
+}
+
+export default ScoreTable;
--- frontend/src/pages/ScoresHome/ScoresHome.js
+++ frontend/src/pages/ScoresHome/ScoresHome.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ScoreTable from '../../components/ScoreTable';
 import SearchBar from '../../components/SearchBar';
 
 /**
@@ -7,24 +8,83 @@ import SearchBar from '../../components/SearchBar';
   *   (links to Scores)
   */
 class ScoresHome extends Component {
+
+  _isMounted = false;
+
+  /**
+    *   Contructor
+    *   Initializes state
+    */
   constructor() {
     super();
+    this.state = {
+      topScores : [],
+      topScoresLoaded : false
+    }
     this.search = '';
+    this.ScoreLen = 10;
   }
 
+  /**
+    *   After component is mounted, fetch top scores
+    *   Calls fetchTopScores periodically to update displayed top scores
+    */
+  componentDidMount() {
+    this._isMounted = true;
+    this.fetchTopScores();
+    this.updateInterval = setInterval(() => {
+      this.fetchTopScores();
+    }, 10000);
+  }
+
+  /**
+    *   Fetch top scores from backend api
+    */
+  fetchTopScores() {
+    fetch(`/api/scores/top_scores?limit=${this.ScoreLen}`)
+    .then(res => res.json())
+    .then(scores => {
+      if (this._isMounted) {
+        this.setState( {topScores : scores });
+        this.setState( {topScoresLoaded : true });
+        console.log('topScores:', this.state.topScores);
+      }
+    });
+  }
+
+  /**
+    *   Update search from SearchBar
+    *   @param event update event from SearchBar
+    */
   updateSearch(event) {
     this.search = event.target.value;
   };
 
+  /**
+    *   Submit search from SearchBar
+    *   event: not used
+    */
   userSubmit(event) {
     this.props.history.push(`/scores/${this.search}`);
 
   }
 
+  /**
+    *   Render ScoresHome: display top scores and SearchBar
+    */
   render() {
-    return (
-      <SearchBar placeholder='Username' onSubmit={this.userSubmit.bind(this)} onChange={this.updateSearch.bind(this)} />
-    );
+    if (this.state.topScoresLoaded) {
+      return (
+        <div>
+          <ScoreTable data={this.state.topScores} />
+          <SearchBar placeholder='Username' onSubmit={this.userSubmit.bind(this)} onChange={this.updateSearch.bind(this)} />
+        </div>
+      );
+    } else {
+      return (
+        null
+      );
+    }
   }
 
 }
