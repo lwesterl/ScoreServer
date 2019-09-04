c2e4d056825ffe8b89aa9236bf8973b7d3e6334a
lwesterl <lauri.v.westerholm@gmail.com>
2019-09-04 22:23:16 +0300

    add ClickableActions to NavBar

---
+++ frontend/src/components/ClickableAction.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+
+/**
+  *   @class ClickableAction
+  *   Implements an action which is clickable
+  */
+class ClickableAction extends Component {
+
+  /**
+    *   Constructor
+    *   @param props passed from parent
+    *   props.active: initial state, ie. is the item active
+    *   props.href: link destination for the action
+    *   props.onClick: function executed when the item is clicked
+    *   props.parentAction: parent function executed when the item is clicked, executed prior onClick
+    *   props.className: a css class used when not active
+    *   props.activeClassName: a css class used when active
+    */
+  constructor(props) {
+    super(props);
+    const active = this.props.active === undefined ? false : this.props.active;
+    this.state = {
+      active : active
+    }
+  }
+
+  /**
+    *   Set state active
+    */
+  activate() {
+    this.setState({active : true});
+  }
+
+  /**
+    *   Set state not active
+    */
+  deactivate() {
+    this.setState({active: false});
+  }
+
+  /**
+    *   Action executed when item is clicked
+    *   Calls possible props.parentAction and props.onClick
+    */
+  clicked() {
+    if (this.props.parentAction) this.props.parentAction();
+    this.activate();
+    if (this.props.onClick) this.props.onClick();
+  }
+
+  /**
+    *   Render based on the active state
+    */
+  render() {
+    if (this.state.active) {
+      return (
+        <a href={this.props.href} onClick={this.clicked.bind(this)} className={this.props.activeClassName}>{this.props.name}</a>
+      );
+    } else {
+      return (
+        <a href={this.props.href} onClick={this.clicked.bind(this)} className={this.props.className}>{this.props.name}</a>
+      )
+    }
+  }
+
+}
+
+export default ClickableAction;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ClickableAction from './ClickableAction';
 
 
 /**
@@ -8,15 +9,45 @@ import React, { Component } from 'react';
   */
 class NavBar extends Component {
 
-  render() {
-    var navs = []
+  /**
+    *   Constructor
+    *   @param props passed from parent
+    *   props.className: a css class used
+    */
+  constructor(props) {
+    super(props);
+    this.clickableActionRefs = [];
+    this.navs = []
     this.props.items.forEach(item => {
-      navs.push(<li key={item.name}><a href={item.href} onClick={item.onClick}>{item.name}</a></li>)
+      this.navs.push(<li key={item.name} className={this.props.className}><ClickableAction ref={this.setRef} href={item.href} onClick={item.onClick} name={item.name} active={item.active} className='scoreNav' activeClassName='activatedScoreNav' parentAction={this.deactivateClickables.bind(this)}/></li>)
     });
+  }
+
+  /**
+    *   Connect refs to ClickableActions
+    */
+  setRef = (ref) => {
+    this.clickableActionRefs.push(ref);
+  };
+
+  /**
+    *   Deactivate all ClickableActions
+    *   This should be passed as parentAction to ClickableActions
+    */
+  deactivateClickables() {
+    this.clickableActionRefs.forEach(ref => {
+      ref.deactivate();
+    });
+  }
+
+  /**
+    *   Render NavBar
+    */
+  render() {
     return (
-      <div>
-        <ul id='nav'>
-          {navs}
+      <div className={this.props.className}>
+        <ul id='nav' className={this.props.className}>
+          {this.navs}
         </ul>
       </div>
     );
