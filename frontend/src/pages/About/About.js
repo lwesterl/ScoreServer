51c333a7a1efd1d8ef0e6aa20377c1e09ea089d7
lwesterl <lauri.v.westerholm@gmail.com>
2019-09-05 00:42:01 +0300

    add content to the About page

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import HideComponent from '../../components/HideComponent';
+import './About.css';
 
+const content = [
+<p className='about'>
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>,
+<p className='about'><a href='https://github.com/lwesterl/ScoreServer' className='about'>ScoreServer on GitHub</a></p>]
+
+/**
+  *   Render About page
+  */
 function About() {
   return (
     <div>
-      <h1>About</h1>
-      < HideComponent components={[<h2>hello</h2>, <p>Hello hello something text</p>]} />
+      <div>
+        <h1>About</h1>
+        < HideComponent components={content} />
+      </div>
+      <div className='copyright'>
+        © Lauri Westerholm 2019
+      </div>
     </div>
   );
 
