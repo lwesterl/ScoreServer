538a7f79d310d29d37df53fc4233ab520c35fe31
lwesterl <lauri.v.westerholm@gmail.com>
2019-09-14 19:49:39 +0300

    adjust about page styling

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -10,7 +10,7 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>,
-<p className='about'><a href='https://github.com/lwesterl/ScoreServer' className='about'>ScoreServer on GitHub</a></p>]
+<p className='about'><a href='https://github.com/lwesterl/ScoreServer' target='_blank'  rel='noopener noreferrer' className='about'>ScoreServer on GitHub</a></p>]
 
 /**
   *   Render About page
